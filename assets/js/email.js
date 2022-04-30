a8d45b4c2b40bdc98b59a414a7b878f22012215e
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-04-30 16:48:17 +0530

    adssa

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,33 +7,32 @@
       let name = document.getElementById('form_name').value
       let email = document.getElementById('form_email').value
       let subject = document.getElementById('subject_mail').value
-      let message = document.getElementById('message_text').value
-      email1()
-      async function email1() {
-        mode: 'no-cors'
-        try {
-          let data = {
-            name: name,
-            email: email,
-            subject: subject,
-            text: message,
-          }
-          data = JSON.stringify(data)
-          let res = await fetch(
-            'https://emailsmtpserver.herokuapp.com/shoaib',
-            {
-              method: 'POST',
-              body: data,
-              headers: {
-                'content-Type': 'application/json',
-              },
-            }
-          )
-          let data1 = await res.json()
-          console.log({ data: data1 })
-        } catch (error) {
-          console.log({ err: error.message })
-        }
-      }
+      let text = document.getElementById('message_text').value
+      email1(name, email, subject, text)
     })
+
+  async function email1(name, email, subject, text) {
+    try {
+      let data = {
+        name: name,
+        email: email,
+        subject: subject,
+        text: text,
+      }
+      let dataString = JSON.stringify(data)
+      console.log(data, typeof data)
+      let res = await fetch('https://emailsmtpserver.herokuapp.com/shoaib', {
+        mode: 'no-cors',
+        method: 'POST',
+        body: dataString,
+        headers: {
+          'content-Type': 'application/json',
+        },
+      })
+      let data1 = await res.json()
+      console.log({ data: data1 })
+    } catch (error) {
+      console.log({ err: error.message })
+    }
+  }
 }
