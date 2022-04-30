08d75b84e4d318bb4cea44bed249e6985a446823
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-04-30 17:21:33 +0530

    add

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,6 @@
 {
   // contact me form code written in this block
-
+  /*
   document
     .getElementById('send_message_click')
     .addEventListener('click', () => {
@@ -35,4 +35,32 @@
       console.log({ err: error.message })
     }
   }
+*/
+
+  document
+    .getElementById('send_message_click')
+    .addEventListener('click', async () => {
+      try {
+        let data = {
+          name: document.getElementById('form_name').value,
+          email: document.getElementById('form_email').value,
+          subject: document.getElementById('subject_mail').value,
+          text: document.getElementById('message_text').value,
+        }
+        // let dataString = JSON.Str(data)
+        console.log(data, typeof data)
+        let res = await fetch('https://emailsmtpserver.herokuapp.com/shoaib', {
+          mode: 'no-cors',
+          method: 'POST',
+          body: data,
+          headers: {
+            'content-Type': 'application/json',
+          },
+        })
+        let data1 = await res.json()
+        console.log({ data: data1 })
+      } catch (error) {
+        console.log({ err: error.message })
+      }
+    })
 }
