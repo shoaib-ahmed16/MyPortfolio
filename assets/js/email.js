89454b3cb8232f15a96934816daab3997a97e4a3
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-11 05:58:13 +0530

    adsasda

--- assets/js/email.js
+++ assets/js/email.js
@@ -37,32 +37,31 @@
   }
 */
 
-  document
-    .getElementById('send_message_click')
-    .addEventListener('click', async () => {
-      try {
-        let data = {
-          name: document.getElementById('form_name').value,
-          email: document.getElementById('form_email').value,
-          subject: document.getElementById('subject_mail').value,
-          text: document.getElementById('message_text').value,
-        }
-        let dataString = JSON.stringify(data)
-        console.log(data, typeof data)
-        console.log(dataString, typeof dataString)
-
-        let res = await fetch('', {
-          mode: 'no-cors',
-          method: 'POST',
-          body: dataString,
-          headers: {
-            'content-Type': 'application/json',
-          },
-        })
-        let data1 = await res.json()
-        console.log({ data: data1 })
-      } catch (error) {
-        console.log({ err: error.message })
+  document.getElementById('form').addEventListener('submit', async (e) => {
+    e.preventDefault()
+    try {
+      let data = {
+        name: document.getElementById('form_name').value,
+        email: document.getElementById('form_email').value,
+        subject: document.getElementById('subject_mail').value,
+        text: document.getElementById('message_text').value,
       }
-    })
+      let dataString = JSON.stringify(data)
+      console.log(data, typeof data)
+      console.log(dataString, typeof dataString)
+
+      // let res = await fetch('', {
+      //   mode: 'no-cors',
+      //   method: 'POST',
+      //   body: dataString,
+      //   headers: {
+      //     'content-Type': 'application/json',
+      //   },
+      // })
+      // let data1 = await res.json()
+      // console.log({ data: data1 })
+    } catch (error) {
+      console.log({ err: error.message })
+    }
+  })
 }
